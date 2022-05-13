b049ccb4221d25c593d19b614f74a1431820a819
Cory Simpson <cory.simpson.dev@gmail.com>
2022-05-13 15:34:56 -0700

    challenge05132022 8kyu conver a boolean to a string

---
+++ challenge051322.js
@@ -0,0 +1,11 @@
+// 8kyu - Convert a Boolean to a String
+
+// P - take in boolean
+// R - return string
+// E - take in true, return 'true'
+
+// set up function
+function booleanToString(b) {
+    // convert argument to string & return
+    return b.toString()
+}
