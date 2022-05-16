5c461bf9673e6207c3e1683e1b2c023262e29698
Cory Simpson <cory.simpson.dev@gmail.com>
2022-05-15 17:22:56 -0700

    8kyu convert a string to a number

---
+++ challenge051522-1.js
@@ -0,0 +1,10 @@
+// 8kyu - Convert a string to a number
+
+// P - string representing integral number
+// R - number
+// E - '1234' => 1234
+
+const stringToNumber = function(str) {
+    // return str as number
+    return +str
+}
