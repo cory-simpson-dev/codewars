6067c479e86545c58eae1419c065f351eba89e4f
Cory Simpson <cory.simpson.dev@gmail.com>
2022-05-18 23:07:06 -0700

    7kyu sum of odd numbers

---
+++ challenge051822-4.js
@@ -0,0 +1,8 @@
+// 7kyu - sum of odd numbers
+
+// take in triangle of consecutive odd numbers
+// return sum of the numbers in the nth row starting with index 1
+// 2 => 8 (i.e. 3+5)
+function rowSumOddNumbers(n) {
+    return n*n*n
+}
