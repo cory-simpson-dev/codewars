e58b5e20b248364dd04af5d78963d321c6c52f0e
Cory Simpson <cory.simpson.dev@gmail.com>
2022-05-19 14:15:31 -0700

    8kyu returning strings

---
+++ challenge051922-5.js
@@ -0,0 +1,9 @@
+// 8kyu - Returning strings
+
+// take in string
+// return string template literal
+// "Ryan" => "Hello, Ryan how are you doing today?"
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`
+}
