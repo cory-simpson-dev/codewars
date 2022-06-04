a72963865617cbedede907857aec141ddfbc0dc8
Cory Simpson <cory.simpson.dev@gmail.com>
2022-06-04 14:30:51 -0700

    8kyu keep hydrated!

---
+++ challenge060422-2.js
@@ -0,0 +1,9 @@
+// 8kyu - Keep hydrated!
+
+// take in time (hour integer may have decimals)
+// return integer (rounded down) of time/2
+// 2 => 1
+
+function litres(time) {
+    return Math.floor(time/2);
+}
