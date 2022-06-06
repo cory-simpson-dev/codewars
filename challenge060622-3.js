537cfcc62d5f589629a9e2b460a7e7d3855800df
Cory Simpson <cory.simpson.dev@gmail.com>
2022-06-06 08:50:37 -0700

    8kyu my head is at the wrong end!

---
+++ challenge060622-3.js
@@ -0,0 +1,9 @@
+// 8kyu - my head is at the wrong end!
+
+// take in array
+// return array with 1st and 3rd ele swapped
+// ["tail", "body", "head"] => ["head", "body", "tail"]
+
+function fixTheMeerkat(arr) {
+    return arr.reverse();
+}
